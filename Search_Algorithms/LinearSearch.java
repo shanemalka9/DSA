83404c6bccba7140dc9a648de1447d79077678db
Ambagahage Shan Emalka Fernando <154874345+shanemalka9@users.noreply.github.com>
2025-03-30 12:03:11 +0000

    Implimented Lenier Search

---
+++ Search_Algorithms/LinearSearch.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+class LinearSearch {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int[] nums = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        
+        System.out.print("Enter value to search: ");
+        int target = input.nextInt();
+        linearSearch(nums, target);
+    }
+
+    private static void linearSearch(int[] nums, int target){
+        for(int  i = 0;  i < nums.length; i++) {
+            if (nums[i] == target) {
+                System.out.println("Target is at " + i + " index.");
+            }
+        }
+        System.out.println("Value Not Found");
+    }
+}
