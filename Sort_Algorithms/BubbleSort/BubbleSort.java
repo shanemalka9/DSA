6c85106546408bb6a6cfbbc4979b50b96b033c56
Ambagahage Shan Emalka Fernando <154874345+shanemalka9@users.noreply.github.com>
2025-04-01 10:40:53 +0000

    Implemented Bubble Sort

---
+++ Sort_Algorithms/BubbleSort/BubbleSort.java
@@ -0,0 +1,33 @@
+package Sort_Algorithms.BubbleSort;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] nums = { 6, 5, 2, 8, 9, 3, 4 };
+
+        System.out.println("Final output : " + Arrays.toString(bubbleSort(nums)));
+    }
+
+    private static int[] bubbleSort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) { // Loop through thea array multiple times to sort
+            boolean sorted = true;
+
+            for (int j = 0; j < arr.length - i - 1; j++) { // Loop through the elements to swap
+                if (arr[j] > arr[j + 1]) {
+                    sorted = false;
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+
+            System.out.println(Arrays.toString(arr));
+
+            if (sorted) {
+                return arr;
+            }
+        }
+        return arr;
+    }
+}
