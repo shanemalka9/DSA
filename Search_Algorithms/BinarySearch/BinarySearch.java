e29c8e617626e5fc1d73882481ac84eba0dd9b87
Ambagahage Shan Emalka Fernando <154874345+shanemalka9@users.noreply.github.com>
2025-04-01 09:09:46 +0000

    Implemented Binary search using recurssion

--- Search_Algorithms/BinarySearch/BinarySearch.java
+++ Search_Algorithms/BinarySearch/BinarySearch.java
@@ -14,26 +14,44 @@ public class BinarySearch {
         int target = input.nextInt();
         input.close();
 
-        int output = binarySearch(nums, target); // Call the method and returns a value
+        int output = binarySearch(nums, target, true); // Call the method and returns a value
         System.out.println(output); // Prints the value
     }
 
-    private static int binarySearch(int[] nums, int target) {
+    private static int binarySearch(int[] nums, int target, boolean rec) {
         // Initialize the fisrt and last values
         int first = 0;
         int last = nums.length - 1;
 
-        while (first <= last) {
+        if (rec) {
+            return binarySearchRecurssion(nums, target, first, last);
+        } else {
 
-            int mid = ((first + last) / 2); // Initialize the middle value
+            while (first <= last) {
 
-            if (nums[mid] == target) { // If middle return mid
-                return mid;
-            } else if (target > nums[mid]) { // If target greater than middle make mid + 1 as first
-                first = mid + 1;
-            } else {
-                last = mid - 1; //  else make mid  - 1 as last
+                int mid = ((first + last) / 2); // Initialize the middle value
+
+                if (nums[mid] == target) { // If middle return mid
+                    return mid;
+                } else if (target > nums[mid]) { // If target greater than middle make mid + 1 as first
+                    first = mid + 1;
+                } else {
+                    last = mid - 1; // else make mid - 1 as last
+                }
             }
+            return -1;
+        }
+    }
+
+    private static int binarySearchRecurssion(int[] nums, int target, int first, int last) {
+        int mid = ((first + last) / 2); // Initialize the middle value
+
+        if (nums[mid] == target) { // If middle return mid
+            return mid;
+        } else if (target > nums[mid]) { // If target greater than middle make mid + 1 as first
+            binarySearchRecurssion(nums, target, mid + 1, last);
+        } else {
+            binarySearchRecurssion(nums, target, first, mid - 1); // else make mid - 1 as last
         }
         return -1;
     }
