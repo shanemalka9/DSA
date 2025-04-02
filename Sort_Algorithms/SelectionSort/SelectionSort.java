a769f5d0344e33817d0bc2f49c406ebd0ca03adc
Ambagahage Shan Emalka Fernando <154874345+shanemalka9@users.noreply.github.com>
2025-04-02 05:36:04 +0000

    Implemented Selection Sort

---
+++ Sort_Algorithms/SelectionSort/SelectionSort.java
@@ -0,0 +1,54 @@
+package Sort_Algorithms.SelectionSort;
+
+import java.util.Arrays;
+
+public class SelectionSort {
+    public static void main(String[] args) {
+        int[] nums = {6, 5, 2, 8, 9, 3, 4};
+
+        System.out.println("Final output of minSelectionSort: " + Arrays.toString(minSelectionSort(nums.clone())));
+        System.out.println("Final output of maxSelectionSort: " + Arrays.toString(maxSelectionSort(nums.clone())));
+    }
+
+    private static int[] minSelectionSort(int[] arr) {
+        System.out.println("Unsorted array: " + Arrays.toString(arr));
+        int n = arr.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            if (minIndex != i) {
+                swap(arr, i, minIndex);
+            }
+            System.out.println(Arrays.toString(arr));
+        }
+        return arr;
+    }
+
+    private static int[] maxSelectionSort(int[] arr) {
+        System.out.println("Unsorted array: " + Arrays.toString(arr));
+        int n = arr.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            int maxIndex = 0;
+            for (int j = 1; j < n - i; j++) {
+                if (arr[j] > arr[maxIndex]) {
+                    maxIndex = j;
+                }
+            }
+            swap(arr, maxIndex, n - 1 - i);
+            System.out.println(Arrays.toString(arr));
+        }
+        return arr;
+    }
+
+    private static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
