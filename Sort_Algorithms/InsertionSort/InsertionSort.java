e7dfc9bb28e5bb390f3a005e7ea8c3712d483057
Ambagahage Shan Emalka Fernando <154874345+shanemalka9@users.noreply.github.com>
2025-04-02 13:34:28 +0000

    Implimented insertion Sort

---
+++ Sort_Algorithms/InsertionSort/InsertionSort.java
@@ -0,0 +1,28 @@
+package Sort_Algorithms.InsertionSort;
+
+import java.util.Arrays;
+
+public class InsertionSort {
+    public static void main(String[] args) {
+        int[] nums = { 6, 5, 2, 8, 9, 3, 4 };
+
+        System.out.println("Sorted array using insertion sort : " + Arrays.toString( insertionSort(nums)));
+    }
+
+    private static int[] insertionSort(int[] arr) {
+        System.out.println("Unsorted array: " + Arrays.toString(arr));
+        int n = arr.length;
+
+        for (int i = 1; i < n; i++) { // Loop through the array fom position 1
+            int key = arr[i]; // Add the value at position 1 as a key
+            int j = i - 1; // create a vaiable j which is 1 less than i
+            while (j >= 0 && arr[j] > key) { // Iterate if j is greater than i and j greater than or equal to zero
+                arr[j + 1] = arr[j]; // shift value to the right
+                j--; // reduce j
+            }
+            arr[j + 1] = key; // add key to empty position
+            System.out.println(Arrays.toString(arr));
+       }
+       return arr;
+    }
+}
